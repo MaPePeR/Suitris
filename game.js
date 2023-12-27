9328248b5f0ee68ff3009b7ada763bd5dab68650
MaPePeR <MaPePeR@users.noreply.github.com>
2023-12-27 22:25:16 +0100

    First version without combining

---
+++ game.js
@@ -0,0 +1,249 @@
+const groundBox = new Object()
+
+class Box {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.fixedIndex = null;
+    }
+}
+
+function swapOut(arr, idx) {
+    if (idx == arr.length - 1) {
+        arr.pop()
+    } else {
+        arr[idx] = arr.pop()
+    }
+}
+
+class GameState {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.board = new Array(width * height);
+        this.board.fill(null)
+        this.fixedBoxes = [];
+        this.fallingBox = null;
+        this.gravityBoxes = [];
+        this.growingBoxes = [];
+        this.running = false;
+    }
+
+    start() {
+        this.running = true;
+        this.interval = setInterval(this.nextTick.bind(this), 500);
+    }
+
+    nextBox() {
+        if (this.fallingBox) {
+            throw "Invalid state. Already have falling box";
+        }
+        const size = 1 + Math.floor(Math.random() * 4)
+        this.fallingBox = new Box(Math.floor(this.width / 2) - Math.floor(size / 2), 0, size);
+    }
+
+    canFall(box) {
+        if (box.y + box.size == this.height) {
+            return false;
+        }
+        const h = (box.y + box.size) * this.width
+        for (let x = box.x; x < box.x + box.size; ++x) {
+            if (this.board[h + x]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    insertBoxIntoBoard(box) {
+        if (box.fixedIndex !== null) {
+            throw "Box is already in board"
+        }
+        this.fixedBoxes.push(box)
+        box.fixedIndex = this.fixedBoxes.length - 1
+        for (let i = 0; i < box.size; ++i) {
+            for (let j = 0; j < box.size; ++j) {
+                this.board[(box.y + i) * this.width + box.x + j] = box;
+            }
+        }
+    }
+
+    removeBoxFromBoard(box) {
+        if (box.fixedIndex === null) return
+        if (box.fixedIndex == this.fixedBoxes.length - 1) {
+            this.fixedBoxes.pop()
+        } else {
+            const swapBox = this.fixedBoxes.pop()
+            swapBox.fixedIndex = box.fixedIndex
+            this.fixedBoxes[box.fixedIndex] = swapBox
+        }
+        box.fixedIndex = null;
+        for (let i = 0; i < box.size; ++i) {
+            for (let j = 0; j < box.size; ++j) {
+                this.board[(box.y + i) * this.width + box.x + j] = null;
+            }
+        }
+    }
+
+    getNeighbors(start, stride, end) {
+        const neighbors = [];
+        if (start < this.board.length && end >= 0) {
+            end = Math.min(end, this.board.length)
+            for (let pos = start; pos < end; pos += stride) {
+                if (0 <= pos && pos <= this.board.length && this.board[pos]) {
+                    neighbors.push(this.board[pos]);
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    getTopNeighbors(box) {
+        return this.getNeighbors((box.y - 1) * this.width + box.x, 1, (box.y - 1) * this.width + box.size);
+    }
+
+    getBottomNeighbors(box) {
+        return this.getNeighbors((box.y + box.size) * this.width + box.x, 1, (box.y + box.size) * this.width + box.size);
+    }
+
+    getLeftNeighbors(box) {
+        return this.getNeighbors(box.y * this.width + box.x - 1, this.width, (box.y + box.size) * this.width);
+    }
+
+    getRightNeighbors(box) {
+        return this.getNeighbors(box.y * this.width + box.x + box.size, this.width, (box.y + box.size) * this.width);
+    }
+
+    getSameSizeTouchingBoxes(box) {
+        // TODO: Only touching if touching along half of their size
+        const touching = [];
+        
+        if (box.x > 0) {
+            for (let y = box.y; y < box.y + box.size; ++y) {
+                const other = this.board[y * this.width + box.x - 1]
+                if (other && other.size == box.size) {
+                    touching.push(other)
+                }
+            }
+        }
+        if (box.x + box.size < this.width - 1) {
+            for (let y = box.y; y < box.y + box.size; ++y) {
+                const other = this.board[y * this.width + box.x + box.size]
+                if (other && other.size == box.size) {
+                    touching.push(other)
+                }
+            }
+        }
+        if (box.y + box.size < this.height - 1) {
+            for (let x = box.x; x < box.x + box.size; ++x) {
+                const other = this.board[(box.y + box.size + 1) * this.width + x]
+                if (other && other.size == box.size) {
+                    touching.push(other)
+                }
+            }
+        }
+        if (box.y > 0) {
+            for (let x = box.x; x < box.x + box.size; ++x) {
+                const other = this.board[(box.y - 1) * this.width + x]
+                if (other && other.size == box.size) {
+                    touching.push(other)
+                }
+            }
+        }
+        if (touching.length > 0) {
+            touching.push(box)
+            console.log("Touching", touching)
+        }
+        return touching
+    }
+
+    combineBoxes(boxes) {
+        let center_x = 0;
+        let center_y = 0;
+        boxes.forEach(box => {
+            center_x += box.x;
+            center_y += box.y;
+            this.removeBoxFromBoard(box)
+        })
+        const newsize = boxes[0].size + boxes.length - 1;
+        if (newsize > 11) {
+            return
+        }
+        const newBox = new Box(Math.round(center_x / boxes.length), Math.round(center_y / boxes.length), 1);
+        newBox.targetSize = newsize;
+        this.growingBoxes.push(newBox)
+    }
+
+    move(direction) {
+        if (this.fallingBox) {
+            const newx = Math.max(0, Math.min(this.width - this.fallingBox.size, this.fallingBox.x + direction))
+            if (this.fallingBox.x != newx) {
+                this.fallingBox.x = newx;
+                const touching = this.getSameSizeTouchingBoxes(this.fallingBox);
+                if (touching.length > 0) {
+                    this.combineBoxes(touching);
+                    this.fallingBox = null;
+                }
+            }
+        }
+    }
+
+    moveDown() {
+        if (this.fallingBox) {
+            if (this.canFall(this.fallingBox)) {
+                this.fallingBox.y += 1
+                const touching = this.getSameSizeTouchingBoxes(this.fallingBox);
+                if (touching.length > 0) {
+                    this.combineBoxes(touching);
+                    this.fallingBox = null;
+                }
+            }
+        }
+    }
+
+    growBox(box) {
+
+    }
+
+    nextTick() {
+        console.log("tick", this.fallingBox, this.gravityBoxes, this.fixedBoxes)
+        console.log(this.board.slice((this.height - 1 ) * this.width))
+        if (this.fallingBox) {
+            if (this.canFall(this.fallingBox)) {
+                this.fallingBox.y +=1;
+                const touching = this.getSameSizeTouchingBoxes(this.fallingBox);
+                if (touching.length > 0) {
+                    console.log("Falling box touched something")
+                    this.combineBoxes(touching)
+                    this.fallingBox = null;
+                }
+            } else {
+                this.insertBoxIntoBoard(this.fallingBox);
+                this.fallingBox = null;
+            }
+        } else if (this.growingBoxes.length > 0) {
+            for (let i = 0; i < this.growingBoxes.length; ++i) {
+                this.growBox(this.growingBoxes[i])
+            }
+        } else if (this.gravityBoxes.length > 0) {
+            console.log("Mutliple boxes are falling")
+            for (let i = 0; i < this.gravityBoxes.length; ++i) {
+                let box = this.gravityBoxes[i];
+                if (this.canFall(box)) {
+                    box.y += 1;
+                    const touching = this.getSameSizeTouchingBoxes(this.fallingBox);
+                    if (touching.length > 0) {
+                        this.combineBoxes(touching);
+                    }
+                } else {
+                    this.insertBoxIntoBoard(box)
+                    swapOut(this.gravityBoxes, i)
+                    --i;
+                }
+            }
+        } else if (this.running) {
+            this.nextBox();
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,156 @@
+const GAME_WIDTH = 15
+const GAME_HEIGHT = 20
+const sizes = [1,2,3,4,5,6,7,8,9,10,11]
+const colors = [
+    '#ff2813ff',
+    '#ff0059ff',
+    '#9e13ffff',
+    '#ffd313ff',
+    '#ff7313ff',
+    '#ff1318ff',
+    '#00ff69ff',
+    '#ff5fd4ff',
+    '#b4ff13ff',
+    '#00d654ff',
+    '#028000ff',
+]
+
+const PIX_PER_BOX = 600 / GAME_WIDTH
+
+async function getEl(id) {
+    const el = document.getElementById(id)
+    if (el) {
+        return el;
+    }
+    throw "Could not find element with id " + id
+}
+
+async function getCtx(promise, options) {
+    const canvas = await promise
+    if (canvas instanceof HTMLCanvasElement) {
+        const context = canvas.getContext("2d", options);
+        if (context instanceof CanvasRenderingContext2D) {
+            return context
+        }
+    }
+    throw "Could not get Context"
+}
+
+let current_game = null;
+
+let lastLog = 0
+let lastRender = 0
+
+/**
+ * 
+ * @param CanvasRenderingContext2D ctx 
+ * @param Box box 
+ */
+function drawBox(ctx, box, state) {
+    ctx.strokeStyle = 'black'
+    ctx.fillStyle = colors[box.size - 1]
+    if (state == 'growing') {
+        ctx.beginPath();
+        ctx.roundRect(box.x * PIX_PER_BOX, box.y * PIX_PER_BOX, box.size * PIX_PER_BOX, box.size * PIX_PER_BOX, [5]);
+        ctx.fill()
+        ctx.stroke()
+    } else {
+        ctx.fillRect(box.x * PIX_PER_BOX, box.y * PIX_PER_BOX, box.size * PIX_PER_BOX, box.size * PIX_PER_BOX)
+        ctx.strokeRect(box.x * PIX_PER_BOX, box.y * PIX_PER_BOX, box.size * PIX_PER_BOX, box.size * PIX_PER_BOX)
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (!current_game) return;
+    if (e.code === "ArrowLeft") {
+        current_game.move(-1);
+    } else if (e.code === "ArrowRight") {
+        current_game.move(1);
+    } else if (e.code === "ArrowDown") {
+        current_game.moveDown();
+    }
+    lastRender = 0
+});
+
+function setup() {
+    const p_canvas_bg = getEl('gamebg');
+    const p_canvas_fg = getEl('gamefg');
+    const p_context_bg = getCtx(p_canvas_bg, { alpha: false } );
+    const p_context_fg = getCtx(p_canvas_fg, null);
+
+    const p_f_draw_fg = (async () => {
+        const ctx = await p_context_fg
+        return () => {
+            if (!current_game) return
+            ctx.clearRect(0, 0, GAME_WIDTH * PIX_PER_BOX, GAME_HEIGHT * PIX_PER_BOX)
+            if (current_game.fallingBox) {
+                drawBox(ctx, current_game.fallingBox, 'falling')
+            }
+            current_game.gravityBoxes.forEach(box => {
+                drawBox(ctx, box, 'gravity')
+            });
+            current_game.growingBoxes.forEach(box => {
+                drawBox(ctx, box, 'growing')
+            });
+        }
+    })();
+
+    const p_f_draw_bg = (async () => {
+        const ctx = await p_context_bg
+        return () => {
+            if (!current_game) return;
+            ctx.fillStyle = 'white'
+            ctx.fillRect(0, 0, GAME_WIDTH * PIX_PER_BOX, GAME_HEIGHT * PIX_PER_BOX)
+            current_game.fixedBoxes.forEach(box => {
+                drawBox(ctx, box, 'fixed')
+            })
+            /*ctx.strokeStyle = 'black';
+            for (let y = GAME_HEIGHT - 1; y < GAME_HEIGHT; ++y ) {
+                for (let x = 0; x < GAME_WIDTH; ++x) {
+                    const cell = current_game.board[y * GAME_WIDTH + x]
+                    console.log(cell)
+                    if (cell instanceof Box) {
+                        ctx.moveTo(x * PIX_PER_BOX, y * PIX_PER_BOX);
+                        ctx.lineTo((x+1) * PIX_PER_BOX, (y+1) * PIX_PER_BOX);
+                        ctx.moveTo(x * PIX_PER_BOX, (y+1) * PIX_PER_BOX);
+                        ctx.lineTo((x+1) * PIX_PER_BOX, y * PIX_PER_BOX);
+                        ctx.stroke();
+                        ctx.strokeText(cell.fixedIndex + ' ' + cell.size, (x + 0.5) * PIX_PER_BOX, (y + 0.5) * PIX_PER_BOX )
+                    }
+                }
+            }*/
+        }
+    })();
+
+    const p_f_render = (async () => {
+        const draw_fg = await p_f_draw_fg
+        const draw_bg = await p_f_draw_bg
+        return function render(timestamp) {
+            if (timestamp - lastRender < 42) {
+                requestAnimationFrame(render);
+                return; // Framerate < 24Hz
+            }
+            if (timestamp - lastLog > 1000) {
+                //console.log("Render...");
+                lastLog = timestamp;
+            }
+            lastRender = timestamp
+            draw_fg()
+            draw_bg()
+            requestAnimationFrame(render);
+        }
+    })()
+
+    const p_start_button = getEl('startbutton');
+    (async () => {
+        const button = await p_start_button;
+        const f_render = await p_f_render;
+        button.addEventListener('click', () => {
+            button.style.display = 'none';
+            current_game = new GameState(GAME_WIDTH, GAME_HEIGHT);
+            current_game.start();
+            requestAnimationFrame(f_render)
+        })
+    })();
+}
+setup()
